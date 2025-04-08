911ded70de5e009bf0e5d84229947db987cac520
GUGA <hdjcool@gmail.com>
2025-04-08 11:11:33 +0900

    KeypointPreprocess 오탈자 수정

--- dataset_maker/dataset_maker.py
+++ dataset_maker/dataset_maker.py
@@ -9,7 +9,7 @@ import numpy as np
 import pandas as pd
 from pose_estimator import PoseEstimator3D
 from kalman_filter import KalmanFilterTracker3D
-from kalman_filter import KeypointPreprosess
+from kalman_filter import KeypointPreprocess
 from com_calculator import COMCalculator
 
 class DatasetCreater:
@@ -17,11 +17,11 @@ class DatasetCreater:
         self.pose_estimator = PoseEstimator3D(roi_ratio=roi_ratio, roi_padding=roi_padding)
         self.kalman_tracker = KalmanFilterTracker3D()
         self.com_calculator = COMCalculator()
-        self.keypoint_preprosessing = KeypointPreprosess(True)
-        
+        self.keypoint_preprosessing = KeypointPreprocess(True)
+
         self.video_path = video_path
         self.cut_file = cut_file
-        
+
         # 안정화를 위한 변수
         self.stable_com_pos = None
         self.max_allowed_move = 30.0  # 한 프레임당 최대 허용 이동 거리
@@ -39,7 +39,7 @@ class DatasetCreater:
                     frame_count, label = parts
                     cuts.append((int(frame_count.strip()), label.strip()))
         return cuts
-    
+
     def _calculate_com(self, filtered_keypoints_3d):
         """
         무게중심 계산 및 안정화
@@ -80,7 +80,7 @@ class DatasetCreater:
             self.stable_com_pos = com_3d
 
         return com_3d
-        
+
     def process_frame(self, frame):
         """
         프레임 처리 및 3D 포즈 추적
@@ -91,70 +91,70 @@ class DatasetCreater:
         """
         try:
             landmarks, processed_frame = self.pose_estimator.estimate_pose(frame)
-            
+
             if landmarks:
                 # 3D 키포인트 추출
                 keypoints_3d = self.pose_estimator.extract_3d_keypoints(landmarks, frame)
-                
+
                 # 칼만 필터로 키포인트 필터링
                 filtered_keypoints_3d = self.kalman_tracker.track(keypoints_3d)
-                
+
                 # 3D CoM 계산
                 com_3d = self._calculate_com(filtered_keypoints_3d)
-                
-                keypoints_and_com = filtered_keypoints_3d[11:] 
+
+                keypoints_and_com = filtered_keypoints_3d[11:]
                 keypoints_and_com.append(com_3d)
 
-                keypoints_and_com = self.keypoint_preprosessing.prosess(keypoints_and_com)
+                keypoints_and_com = self.keypoint_preprosessing.process(keypoints_and_com)
                 sequence_data = [x for y in keypoints_and_com for x in y]
-                
+
             return sequence_data
-            
+
         except Exception as e:
             print(f"Error processing frame: {e}")
             traceback.print_exc()
             return frame
-            
+
     def make_label(self, video_path):
         label_list = ['stand', 'forte', 'walk', 'piano']
-        
+
         for labels in label_list:
             if labels in video_path:
                 label = label_list.index(labels)
-                
+
         return label
-        
+
     def run(self):
         """
         비디오 캡처 및 처리
-        
+
         MP4 파일을 프레임 수에 따라 라벨링 후 병합
         """
         # 비디오 파일을 사용하여 캡처
         if not self.video_path:
             print("Error: No video source provided.")
             return
-            
+
         cap = cv2.VideoCapture(self.video_path)
         cuts = self.read_cut_file(self.cut_file)
         sequence_tensor, current_frame = [[] , 0]
-        
+
         if not cap.isOpened():
             print("Error: Could not open video source.")
             return
-        
+
         for frame_count, label in cuts:
             for _ in range(frame_count):
                 ret, frame = cap.read()
-                
+
                 if not ret:
                     print("End of video or failed to capture frame")
                     break
-                    
+
                 sequence_vector = self.process_frame(frame)
                 sequence_vector.append(self.make_label(label))
                 sequence_tensor.append(sequence_vector)
-            
+
         cap.release()
         csv_filename = self.video_path.rsplit('.')[0] + "_dataset.csv"
         df = pd.DataFrame(sequence_tensor)
@@ -168,6 +168,6 @@ if __name__ == "__main__":
     parser.add_argument("video_path", type=str, help="Path to the input video file.")
     parser.add_argument("cut_file", type=str, help="Path to the cut.txt file")
     args = parser.parse_args()
-    
+
     pose_tracking = DatasetCreater(roi_ratio=1, video_path=args.video_path, cut_file=args.cut_file)
     pose_tracking.run()
